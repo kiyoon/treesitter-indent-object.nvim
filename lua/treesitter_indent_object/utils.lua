508f7d5c66cd5234d263eaa43a22097deda96755
WieeRd <verywieerd@gmail.com>
2024-07-06 01:52:25 +0900

    feat: add `include_surrounding_empty_lines()`

--- lua/treesitter_indent_object/utils.lua
+++ lua/treesitter_indent_object/utils.lua
@@ -82,7 +82,6 @@ M.find_indent = function(line_num)
   return indent + math.floor(spaces / shiftwidth)
 end
 
-
 -- Modified from indent_blankline.utils
 -- to return column as well
 M.get_current_context = function(type_patterns, use_treesitter_scope)
@@ -122,6 +121,34 @@ M.get_current_context = function(type_patterns, use_treesitter_scope)
   end
 
   return false
+end
+
+---Expand the linewise visual selection to the surrounding empty lines.
+M.include_surrounding_empty_lines = function()
+  local cur = vim.fn.line "."
+  local eob = vim.fn.line "$"
+
+  -- expand the selection to the empty lines below
+  local i = cur
+  while i < eob and vim.fn.getline(i + 1):match "^%s*$" ~= nil do
+    i = i + 1
+  end
+  vim.api.nvim_win_set_cursor(0, { i, 99999 })
+
+  -- if: there were no empty lines below (cursor did not move)
+  -- or *only* empty lines were below (cursor at the last line)
+  if i ~= cur and i ~= eob then
+    return
+  end
+
+  -- then: also expand the selection to the empty lines above
+  vim.cmd [[normal! o]]
+  local i = vim.fn.line "."
+  while i > 1 and vim.fn.getline(i - 1):match "^%s*$" ~= nil do
+    i = i - 1
+  end
+  vim.api.nvim_win_set_cursor(0, { i, 0 })
+  vim.cmd [[normal! o]]
 end
 
 return M
